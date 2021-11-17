0ff87b6decfcfa8b3b001468d3004c8d14a5d1d7
Nahuelvillagra <63684900+Nahuelvillagra@users.noreply.github.com>
2021-11-17 03:04:08 -0300

    Add files via upload

---
+++ juegoPalabras.js
@@ -0,0 +1,58 @@
+ var palabra = ["auto","agua","jugo","caja","pala"];
+    
+var palabraseleccionada =  palabra[Math.floor(Math.random()*4)];
+
+const elegida = palabraseleccionada;
+
+var arraydepalabra = palabraseleccionada.split('');
+
+console.log(arraydepalabra);
+
+let desordenado=arraydepalabra.sort(() => {
+    return Math.random() - 0.5
+})
+
+var des = desordenado.join('');
+
+console.log(des);
+
+document.getElementById("palUs").innerHTML = des;
+
+document.getElementById("validar").addEventListener("click", function() {
+    var texto = document.getElementById("txt").value;
+    console.log(texto);
+    //colsole.log(elegida);
+    if(texto == elegida){
+        console.log(elegida);
+        document.getElementById("respuesta").innerHTML = "CORRECTO!";
+        document.getElementById("niv").innerHTML = '';
+          document.getElementById("respuesta").innerHTML = '';
+          document.getElementById("palUs").innerHTML = '';
+          document.getElementById("niv").innerHTML = "GANASTE ESTE NIVEL </br> <button><a href='niveldos.php'>Ir al siguiente</a></button>"
+          
+    }else{
+        incorrecta();
+    }
+    
+  });
+  var fija=3;
+  document.getElementById("intentos").innerHTML = fija;
+  
+  function incorrecta(){
+      //var i =document.getElementById("intentos").value;
+      //console.log(typeof(i));
+      document.getElementById("respuesta").innerHTML = "INCORRECTO!";
+      document.getElementById("intentos").innerHTML = fija - 1;
+      fija = fija-1;
+      if(fija == 0){
+          document.getElementById("niv").innerHTML = '';
+          document.getElementById("respuesta").innerHTML = '';
+          document.getElementById("palUs").innerHTML = '';
+          document.getElementById("niv").innerHTML = "PERDISTE </br> <button id='reload'>Volver a empezar</button>"
+          document.getElementById("reload").addEventListener("click", function(){
+            document.location.reload();
+          });
+      }
+  }
+    
+  
---
+++ niveltres.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Challenge teknos - ordena la palabra</title>
+</head>
+<body>
+    <div class="container-fluid" style="text-align: center;">
+        <h1>¡Escriba la palabra correcta para ganar el juego!</h1>
+        <p>Una vez que completa todas las palabras del nivel avanza al siguiente</p>
+        <div class="row">
+            <div class="col-md-12" >
+
+                <div class="col-mb-12" id="niveluno" >
+                    <h2>Nivel 3 - Avanzado</h2>
+                    <p>Intentos restantes</p> <p id="intentos">3</p>
+                
+                    <h3 id="palUs"></h3> <p id="niv"><input id="txt" type="text"> <button id="validar">Validar</button><br></p>
+                    <div id="respuesta"></div>
+                    
+                </div>
+                
+            </div>
+        </div>
+
+        </div>
+    </div>
+</body>
+<script src="niveltres.js"></script>
+</html>
